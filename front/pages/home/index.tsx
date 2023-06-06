8bbc85e3406a5a3e12f25fb58f5c5817ff296c9a
사월 <april35592@gmail.com>
2023-06-06 19:17:49 +0900

    err fix

--- front/pages/home/index.tsx
+++ front/pages/home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Create from '@pages/create';
 import Join from '@pages/join';
+import Room from '@pages/room';
 import Header from '@components/header';
 import NotFound from '@pages/not_found';
 
@@ -10,10 +11,10 @@ const Home = () => {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Create />}></Route>
-        <Route path="/join" element={<Join />}></Route>
-        <Route path="/room/:room_id/:user_id" element={<Room />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="/" element={<Create />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/room/:room_id/:user_id" element={<Room />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
