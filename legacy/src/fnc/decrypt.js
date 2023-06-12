b0966da724aebe08e11d8995d2ea6336dd1ba395
사월 <april35592@gmail.com>
2023-06-12 20:42:37 +0900

    legacy 제거

--- legacy/src/fnc/decrypt.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from "crypto-js";
-
-const decrypt = (string) => {
-  return JSON.parse(
-    CryptoJS.AES.decrypt(string, "").toString(CryptoJS.enc.Utf8)
-  );
-};
-
-export default decrypt;
--- legacy/src/fnc/encrypt.js
+++
@@ -1,7 +0,0 @@
-import CryptoJS from "crypto-js";
-
-const encrypt = (arr) => {
-  return CryptoJS.AES.encrypt(JSON.stringify(arr), "").toString();
-};
-
-export default encrypt;
